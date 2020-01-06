0227752ee0a415198f30f0a7818cde345ae0f4a2
Nejc <kozjek.nejc198@gmail.com>
2020-01-06 19:31:05 +0100

    added routing

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  win.loadFile('mehanikZaloga.html')
+  win.loadFile('index.html')
 
   // Open the DevTools.
   win.webContents.openDevTools()
